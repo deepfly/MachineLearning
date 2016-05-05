44feec536dff29596ccfe25f547404d57028e665
Ziping Zheng <nkuzzp@gmail.com>
2016-05-04 23:01:44 -0400

    nbStopWords.py comment added

--- NaiveBayes/nbStopWords.py
+++ NaiveBayes/nbStopWords.py
@@ -1,13 +1,19 @@
 ########################################################################
 # File: nbStopWords.py
-# Version: 27 April 2016 (ZZ)
+# Version: 4 May 2016 (ZZ)
 #
 # Description:
 #
+#      It will take a set of labeled training examples file list
+#  (formatted like split.train) and a set of test examples files
+#  list(formatted like split.test), and classify them using a 
+#  Naive Bayes classifier.
+#      The toppest n frequent occurred words will be ignored during
+#  counting.
 #
 # Usage:
-#
-#
+#      
+#      python nb.py <split.train> <split.test> <number: top n>
 #
 # @author: Ziping Zheng
 #
@@ -41,19 +47,20 @@ def topwords(n):
 		topn.append(sortedmap[i][0])
 	return topn
 
-todel = topwords(int(sys.argv[3]))
-# print libtodel
-# print contodel
-file = open(sys.argv[1], 'r')
-wordmap = {}
-wordlib = {}
-wordcon = {}
-wcountlib = 0
-wcountcon = 0
-countlib = 0
-countcon = 0
+# todel stores a list of all the top words to ignore in the text files.
+todel = topwords(int(sys.argv[3])) #The param is the number of top words to find.
+
+file = open(sys.argv[1], 'r') #This file is a list of text files.
+wordmap = {} #Store the count for each word
+wordlib = {} #Store the count for each word appeared in lib file
+wordcon = {} #Store the count for each word appeared in con file
+wcountlib = 0 #count of words in all lib files
+wcountcon = 0 #count of words in all con files
+countlib = 0 #count of lib files
+countcon = 0 #count of con files
 isLib = False
-# st = set([])
+
+#collect all words that occur in training text
 while 1:
 	line = file.readline()
 	if not line:
@@ -66,6 +73,7 @@ while 1:
 		isLib = False
 		countcon = countcon + 1
 	file2 = open(line, 'r')
+	#from each file in the list collect the data from the text.
 	while 1:
 		line2 = file2.readline()
 		if not line2:
@@ -73,6 +81,7 @@ while 1:
 		line2 = line2.strip('\r\n').lower()
 		if line2 in todel:
 			continue
+
 		if wordmap.has_key(line2):
 			wordmap[line2] = wordmap[line2] + 1
 		else:
@@ -83,6 +92,8 @@ while 1:
 			else:
 				wordlib[line2] = 1
 
+			# if the word does not appear in lib file, 
+			# it should also be stored in wordlib with count 0.
 			if not wordcon.has_key(line2):
 				wordcon[line2] = 0
 			wcountlib = wcountlib + 1
@@ -92,11 +103,13 @@ while 1:
 			else:
 				wordcon[line2] = 1
 
+			# if the word does not appear in con file, 
+			# it should also be stored in wordcon with count 0.
 			if not wordlib.has_key(line2):
 				wordlib[line2] = 0
 			wcountcon = wcountcon + 1
 
-# print st
+# calculate p(w|v) probability terms
 for (k, v) in wordlib.items():
 	prob = 1.0 * (v + 1) / (wcountlib + len(wordmap))
 	wordlib[k] = prob
@@ -104,9 +117,11 @@ for (k, v) in wordcon.items():
 	prob = 1.0 * (v + 1) / (wcountcon + len(wordmap))
 	wordcon[k] = prob
 
+# calculate p(v)
 plib = 1.0 * countlib / (countcon + countlib)
 pcon = 1.0 - plib
 
+#2. classify naive bayes text
 right = 0
 total = 0
 file = open(sys.argv[2], 'r')
@@ -121,6 +136,8 @@ while 1:
 	else:
 		isLib = False
 	file2 = open(line, 'r')
+
+	# use the MAP to find the more probable classification
 	postlib = math.log(plib)
 	postcon = math.log(pcon)
 	while 1:
@@ -132,8 +149,7 @@ while 1:
 			postlib = postlib + math.log(wordlib[line2])
 		if wordcon.has_key(line2):
 			postcon = postcon + math.log(wordcon[line2])
-	# print "l" + str(postlib)
-	# print "c" + str(postcon) + ","
+	# calculate the accuracy using the label
 	if postcon > postlib:
 		print 'C'
 		if not isLib:
@@ -143,7 +159,6 @@ while 1:
 		if isLib:
 			right = right + 1
 
+# print the accuracy
 accuracy = 1.0 * right / total
-# print right
-# print total
 print ("Accuracy: %.04f"%(accuracy))
